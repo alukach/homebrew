76f4c415c591ed8a846a396774d373df17f6be2c
Anthony Lukach <anthonylukach@gmail.com>
2013-09-11 23:18:01 -0700

    Working on putting Levitus DB in right location

--- Library/Formula/mb-system.rb
+++ Library/Formula/mb-system.rb
@@ -1,38 +1,49 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+# SubFormula as per http://stackoverflow.com/questions/10665072/homebrew-formula-download-two-url-packages
+class LevitusTempSalinityDb < Formula
+  url 'ftp://ftp.ldeo.columbia.edu/pub/MB-System/annual.gz'
+  sha1 '2dd876e3d4a56ac6502f7ff92156f072e54183e7'
+  version 'n/a'
+end
 
 class MbSystem < Formula
   homepage 'http://www.ldeo.columbia.edu/res/pi/MB-System/'
   url 'ftp://ftp.ldeo.columbia.edu/pub/MB-System/mbsystem-5.4.2135.tar.gz'
   sha1 'ea25543df5fcc69477802eb6f7b63646dfb068f8'
 
-  # depends_on 'cmake' => :build
-  depends_on 'gmt'    # Fink had gmt-dev
-  depends_on 'netcdf' # Fink had netcdf-shlibs
-  depends_on 'proj'
-  depends_on 'fftw'   # Fink had fftw3
+  # FROM FINK DIRECTIONS
+  #   BuildDepends: gmt-dev, openmotif4, netcdf, x11-dev, proj, fftw3
+  #   BuildConflicts: lesstif, openmotif3
+  #   Depends: gmt, openmotif4-shlibs, netcdf-shlibs, x11, gv, proj-bin, proj-shlibs
+  # depends_on 'gmt'    # Fink had gmt-dev
+  # depends_on 'netcdf' # Fink had netcdf-shlibs
+  # depends_on 'proj'
+  # depends_on 'fftw'   # Fink had fftw3
   depends_on :x11     # Fink had x11-dev
 
-  conflicts_with 'lesstif', :because => 'Not sure why, this conflict is a carry over from the Fink installation'
-
-  # FROM FINK DIRECTIONS
-  # BuildDepends: gmt-dev, openmotif4, netcdf, x11-dev, proj, fftw3
-  # BuildConflicts: lesstif, openmotif3
-  # Depends: gmt, openmotif4-shlibs, netcdf-shlibs, x11, gv, proj-bin, proj-shlibs
+  conflicts_with 'lesstif', :because => 'This conflict is a carry over from the Fink installation'
 
   def install
+    LevitusTempSalinityDb.new.brew do
+
+      levitus_path = '%s/share/mbsystem' % buildpath
+      # (buildpath/'share/mbsystem').install Dir['*']
+
+      system 'mkdir -p %s' % levitus_path
+    end
+
+
+
     # ENV.j1  # if your formula's build system can't parallelize
 
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    # # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # # system "cmake", ".", *std_cmake_args
+    # system "make", "install" # if this fails, try separate make/make install steps
   end
 
   test do
